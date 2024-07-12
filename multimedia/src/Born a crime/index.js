52ea81ff572aecb08b87857edf26dacc7ed86c01
Faith-wanjiru <wanjirufaithwanyoike@gmail.com>
2024-07-12 20:58:26 +0300

    added blogpost

---
+++ multimedia/src/Born a crime/index.js
@@ -0,0 +1,77 @@
+import "./index.css";
+import logo from "../images/logo.png"
+import novel from "../images/novel.png"
+
+const Book = () =>{
+    return(
+        // <div>
+        //     <div classname=""> 
+        //         <div classname='navbar'> 
+        //         <div classname="logo"> <img src={logo} alt="logo" /></div>
+        //         <div classname="navtop"> 
+        //             <a href="#home">Home</a>
+        //             <a href="#video">Video</a>
+        //             <a href="#Blog">Blog</a>
+        //             <a href="#podcast">Podcast</a>
+        //             <a href="#resume">Resume</a>
+        //         </div>
+        //         </div>
+
+            
+            
+           
+           
+
+        //     <h1 classname="title">Born A Crime</h1>
+        //     <div>
+        //         <p>Born a Crime is a memimor by<br/> comedian Trevor Noah about<br/>his chilhood growing up in<br/>apartheid and post-apartheid<br/>South Africa.<br/> Noah was born to a black mother<br/>and white father, an illegal union<br/>at the time,making his very<br/> existence a criminal act under<br/>the racist apartheid regime  </p>
+        //     </div>
+            
+        //         <image src={novel} alt="novel"></image>
+            
+        //     <button classname="readmore">Read More</button>
+            
+        // </div>
+        //     </div>
+
+
+<div class="">
+        <div class="navbar">
+            <div >
+                <img src={logo} alt="Logo" id="logo"></img>
+            </div>
+            <div class="navtop">
+                <a href="#home">Home</a>
+                <a href="#video">Video</a>
+                <a href="#article">Article</a>
+                <a href="#podcast">Podcast</a>
+                <a href="#resume">Resume</a>
+            </div>
+        </div>
+        
+        <h1 classname="title">Born A Crime</h1>
+        
+            <p>Born a Crime is a memoir by<br/>
+               comedian Trevor Noah about<br/>
+               his childhood growing up in<br/>
+               apartheid and post-apartheid<br/>
+               South Africa.<br/>
+               Noah was born to a black mother<br/>
+               and white father, an illegal union<br/>
+               at the time, making his very<br/>
+               existence a criminal act under<br/>
+               the racist apartheid regime.</p>
+        
+        <div >
+            <img src={novel} alt="Novel" id="trevor"></img>
+        </div>
+       
+        <button class="readmore">Read More</button>
+    </div>
+
+        
+
+    )
+}
+
+export default Book;
