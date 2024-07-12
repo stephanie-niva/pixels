877372abfb347bdefb5a3b1e9302166833a742ab
stephanie-niva <stephanienivaa@example.com>
2024-07-12 15:02:30 +0300

    added podcast

---
+++ multimedia/src/Podcast /index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const Podcast=()=>{
+    return(
+        <div>
+            
+             <section className='landing'>
+            <section className='overlay'>
+             <h1>Laugh,Speak & Be Heard</h1>   
+            </section>
+            </section>
+            
+            
+        </div>
+    )
+}
+
+export default Podcast;
