fa9ce1bc59ab8c91b4504fdbeb6c73435bf02c65
nyabangWech <nyabangwech@gmail.com>
2024-07-12 22:36:14 +0300

    added portfolio

--- multimedia/src/App.js
+++ multimedia/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Portfolio from "./Portfolio"
+import Team from "./Portfolio"
+const App = () => {
+    return (
+      <div>
+         <portfolio/>
+        <Team/>
+      
+      </div>
+    );
+  };
+  export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
---
+++ multimedia/src/Portfolio/index.js
@@ -0,0 +1,111 @@
+import './index.css';
+const Team =()=>{
+    return(
+<div class="container">
+    <p class="fellow">Fellow Profile</p>
+    <div class="Aboutcynthia">
+        <div>
+            <img src="/images/cynthia.png" alt="cynthia"/>
+            {/* <h1>Summary</h1>   */}
+            <p>Cynthia is a self-motivated hardworking lady </p>
+            <p>She love  travelling and exploring</p>
+            <p>she is fascinated by the World of Tech</p>
+            <p>And she would love to connect without techie</p>
+               </div>
+           <div> 
+           <p>Cynthia Matista</p>
+           <p>software</p>
+           <p>Akirachix/Kenya</p>
+           </div>
+           <div> 
+            <div>
+            </div>
+    </div>
+    <div class="Exprience">
+        <h1>Experience</h1>
+        <b>worked as software Developer  for  3 years</b>
+        <b>Engaged in different social communitywork</b>
+        <b>works with passion and enthusiast</b>
+        <b>she loves reading and booking</b>
+        <h1>Hobbies</h1>
+        <b>Loves skating</b>
+        <b>Loves Coding</b> 
+        <b>Enjoys Swimming</b>
+    </div>
+    </div>
+    <div>
+        <button class="button">View cv</button>
+
+        <div>
+            <img src="/images/cynthia.png" alt="cynthia"/>
+            {/* <h1>Summary</h1>   */}
+            <p>Cynthia is a self-motivated hardworking lady </p>
+            <p>She love  travelling and exploring</p>
+            <p>she is fascinated by the World of Tech</p>
+            <p>And she would love to connect without techie</p>
+               </div>
+           <div> 
+           <p>Cynthia Matista</p>
+           <p>software</p>
+           <p>Akirachix/Kenya</p>
+           </div>
+           <div> 
+            <div>
+            </div>
+    </div>
+    <div class="Exprience">
+        <h1>Experience</h1>
+        <b>worked as software Developer  for  3 years</b>
+        <b>Engaged in different social communitywork</b>
+        <b>works with passion and enthusiast</b>
+        <b>she loves reading and booking</b>
+        <h1>Hobbies</h1>
+        <b>Loves skating</b>
+        <b>Loves Coding</b> 
+        <b>Enjoys Swimming</b>
+    </div>
+    </div>
+    <div>
+        <button class="button">View cv</button>
+        <div>
+            <img src="/images/cynthia.png" alt="cynthia"/>
+            {/* <h1>Summary</h1>   */}
+            <p>Cynthia is a self-motivated hardworking lady </p>
+            <p>She love  travelling and exploring</p>
+            <p>she is fascinated by the World of Tech</p>
+            <p>And she would love to connect without techie</p>
+               </div>
+           <div> 
+           <p>Cynthia Matista</p>
+           <p>software</p>
+           <p>Akirachix/Kenya</p>
+           </div>
+           <div> 
+            <div>
+            </div>
+    </div>
+    <div class="Exprience">
+        <h1>Experience</h1>
+        <b>worked as software Developer  for  3 years</b>
+        <b>Engaged in different social communitywork</b>
+        <b>works with passion and enthusiast</b>
+        <b>she loves reading and booking</b>
+        <h1>Hobbies</h1>
+        <b>Loves skating</b>
+        <b>Loves Coding</b> 
+        <b>Enjoys Swimming</b>
+    </div>
+    </div>
+    <div>
+        <button class="button">View cv</button>
+    </div>
+    </div>
+    
+    
+     
+    
+)
+    
+
+}
+export default Team;
